be445136e5d19f8c0a785f68fd5c895e7a1788fd
Max <146020430+maxxi0030@users.noreply.github.com>
2025-11-29 19:05:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // config.js
 
-const supabaseUrl = window.SUPABASE_CONFIG?.url || 'https://your-project.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || 'fallback_key';
+
+const supabaseUrl = 'https://spusbagzllwcijabbost.supabase.co';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwdXNiYWd6bGx3Y2lqYWJib3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwMTE2MjMsImV4cCI6MjA3NzU4NzYyM30.BLECbjr1l4exB7s0xZqygd9N1p8KE4zmyGPH4Rxshnw";
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
