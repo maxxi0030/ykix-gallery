c790d9ab7216c836ba1816021bc4ab8476ecff8b
Max <146020430+maxxi0030@users.noreply.github.com>
2025-11-29 18:44:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'fallback-url'
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'fallback-key'
+const supabaseUrl = '{{ .Env.VITE_SUPABASE_URL }}'
+const supabaseKey = '{{ .Env.VITE_SUPABASE_ANON_KEY }}'
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
