6d578b2e0b1c62c74f768b6188330a5cdc6c612b
Max <146020430+maxxi0030@users.noreply.github.com>
2025-11-29 17:48:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // config.js
-// Настройка Supabase (используется на всех страницах)
 
 const supabaseUrl = 'https://spusbagzllwcijabbost.supabase.co';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwdXNiYWd6bGx3Y2lqYWJib3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwMTE2MjMsImV4cCI6MjA3NzU4NzYyM30.BLECbjr1l4exB7s0xZqygd9N1p8KE4zmyGPH4Rxshnw";
